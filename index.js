6d36fa74740e88f6a1ae212a66e8704bb948bafc
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-04-24 12:49:47 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
+            function cambiaTema(ev){
+                if(ev.target.checked){
+                    document.documentElement.setAttribute('tema', 'light');
+                } else {
+                    document.documentElement.setAttribute('tema', 'dark');
+                }
+            }
+            colorSwitch.addEventListener('change', cambiaTema);
 
 const tipButtons = document.querySelectorAll('.tip-button')
 
