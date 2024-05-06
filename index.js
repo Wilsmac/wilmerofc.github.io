1185f053824968a127ec9435562b5a5ddd8f3ee6
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2024-05-06 15:00:13 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,90 +1,22 @@
+$('#login-button').click(function(){
+  $('#login-button').fadeOut("slow",function(){
+    $("#container").fadeIn();
+    TweenMax.from("#container", .4, { scale: 0, ease:Sine.easeInOut});
+    TweenMax.to("#container", .4, { scale: 1, ease:Sine.easeInOut});
+  });
+});
 
-function sumar() {
-  var num1 = parseInt(document.getElementById("num1").value);
-  var num2 = parseInt(document.getElementById("num2").value);
-  document.getElementById("resultado").innerText = "El resultado es: " + (num1 + num2);
-}
+$(".close-btn").click(function(){
+  TweenMax.from("#container", .4, { scale: 1, ease:Sine.easeInOut});
+  TweenMax.to("#container", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
+  $("#container, #forgotten-container").fadeOut(800, function(){
+    $("#login-button").fadeIn(800);
+  });
+});
 
-function restar() {
-  var num1 = parseInt(document.getElementById("num1").value);
-  var num2 = parseInt(document.getElementById("num2").value);
-  document.getElementById("resultado").innerText = "El resultado es: " + (num1 - num2);
-}
-
-function multiplicar() {
-  var num1 = parseInt(document.getElementById("num1").value);
-  var num2 = parseInt(document.getElementById("num2").value);
-  document.getElementById("resultado").innerText = "El resultado es: " + (num1 * num2);
-}
-
-function dividir() {
-  var num1 = parseInt(document.getElementById("num1").value);
-  var num2 = parseInt(document.getElementById("num2").value);
-  if (num2 === 0) {
-    document.getElementById("resultado").innerText = "No se puede dividir por cero";
-  } else {
-    document.getElementById("resultado").innerText = "El resultado es: " + (num1 / num2);
-  }
-}
-
-
-  const flipCoin = () => {
-    coin.moveLoopCount = 0
-    flipCoinLoop()
-  }
-
-  const resetCoin = () => {
-    coin.style.setProperty('--coin-x-multiplier', 0)
-    coin.style.setProperty('--coin-scale-multiplier', 0)
-    coin.style.setProperty('--coin-rotation-multiplier', 0)
-    coin.style.setProperty('--shine-opacity-multiplier', 0.4)
-    coin.style.setProperty('--shine-bg-multiplier', '50%')
-    coin.style.setProperty('opacity', 1)
-    // Delay to give the reset animation some time before you can click again
-    setTimeout(() => {
-      button.clicked = false
-    }, 300)
-  }
-
-  const flipCoinLoop = () => {
-    coin.moveLoopCount++
-    let percentageCompleted = coin.moveLoopCount / coin.maxMoveLoopCount
-    coin.angle = -coin.maxFlipAngle * Math.pow((percentageCompleted - 1), 2) + coin.maxFlipAngle
-    
-    // Calculate the scale and position of the coin moving through the air
-    coin.style.setProperty('--coin-y-multiplier', -11 * Math.pow(percentageCompleted * 2 - 1, 4) + 11)
-    coin.style.setProperty('--coin-x-multiplier', percentageCompleted)
-    coin.style.setProperty('--coin-scale-multiplier', percentageCompleted * 0.6)
-    coin.style.setProperty('--coin-rotation-multiplier', percentageCompleted * coin.sideRotationCount)
-
-    // Calculate the scale and position values for the different coin faces
-    // The math uses sin/cos wave functions to similate the circular motion of 3D spin
-    coin.style.setProperty('--front-scale-multiplier', Math.max(Math.cos(coin.angle), 0))
-    coin.style.setProperty('--front-y-multiplier', Math.sin(coin.angle))
-
-    coin.style.setProperty('--middle-scale-multiplier', Math.abs(Math.cos(coin.angle), 0))
-    coin.style.setProperty('--middle-y-multiplier', Math.cos((coin.angle + Math.PI / 2) % Math.PI))
-
-    coin.style.setProperty('--back-scale-multiplier', Math.max(Math.cos(coin.angle - Math.PI), 0))
-    coin.style.setProperty('--back-y-multiplier', Math.sin(coin.angle - Math.PI))
-
-    coin.style.setProperty('--shine-opacity-multiplier', 4 * Math.sin((coin.angle + Math.PI / 2) % Math.PI) - 3.2)
-    coin.style.setProperty('--shine-bg-multiplier', -40 * (Math.cos((coin.angle + Math.PI / 2) % Math.PI) - 0.5) + '%')
-
-    // Repeat animation loop
-    if (coin.moveLoopCount < coin.maxMoveLoopCount) {
-      if (coin.moveLoopCount === coin.maxMoveLoopCount - 6) button.classList.add('shrink-landing')
-      window.requestAnimationFrame(flipCoinLoop)
-    } else {
-      button.classList.add('coin-landed')
-      coin.style.setProperty('opacity', 0)
-      setTimeout(() => {
-        button.classList.remove('clicked', 'shrink-landing', 'coin-landed')
-        setTimeout(() => {
-          resetCoin()
-        }, 300)
-      }, 1500)
-    }
-  }
-})
-
+/* Forgotten Password */
+$('#forgotten').click(function(){
+  $("#container").fadeOut(function(){
+    $("#forgotten-container").fadeIn();
+  });
+});
